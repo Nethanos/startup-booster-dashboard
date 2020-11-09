15934c32f6d3c74a230ef342b70ea16b5cfc6289
Nethanos <n.santos1897@gmail.com>
2020-11-09 14:27:26 -0300

    [feat][sidemenu] creating sidemenu skeleton

---
+++ src/components/sidemenu/Sidemenu.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Sidemenu.scss'
+
+
+type SidemenuProps = {
+
+}
+
+export const Sidemenu = (props: SidemenuProps) => {
+
+    return (<> 
+    <div className="sidemenuContent">
+    </div>
+    </>)
+
+}
