a1865265c5a69158dd71621adaecc144fc126e68
Nethanos <n.santos1897@gmail.com>
2020-11-09 14:28:17 -0300

    [feat][searchbar] searchbar skeleton

---
+++ src/components/searchbar/SearchBar.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './SearchBar.scss';
+
+export const SearchBar = () => {
+    return (<>
+    <div className="searchBarContent shadow-sm p-3 mb-5 bg-white rounded">
+        
+    </div>
+    </>)
+}
