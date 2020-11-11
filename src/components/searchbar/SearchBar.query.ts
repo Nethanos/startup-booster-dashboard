e3754ef436ee281e3e878f6745567f193db89308
Nethanos <n.santos1897@gmail.com>
2020-11-10 21:56:37 -0300

    [component][wip] creatigin graphql query

---
+++ src/components/searchbar/SearchBar.query.ts
@@ -0,0 +1,22 @@
+export default function repositoryRequestQuery(owner: string, name: string): string {
+    return `query getDocuments { 
+      repository(owner: "micronaut-projects", name: "micronaut-core") {
+
+        Issues: issues(last: 100, filterBy: {states: CLOSED}) {
+          nodes {
+            createdAt,
+            closedAt
+          }
+        },
+        
+        mergedPullRequests: pullRequests(last: 100, states: MERGED) {
+          nodes {
+            createdAt,
+            mergedAt,
+            additions,
+            deletions
+          }
+        },      
+  		}
+    }`;
+  }
--- src/components/searchbar/SearchBar.tsx
+++ src/components/searchbar/SearchBar.tsx
@@ -1,10 +1,48 @@
-import React from 'react';
-import './SearchBar.scss';
+import React, { FormEvent, useState } from "react";
+import { requestQuery } from "../../middlewares/ApolloClient";
+import repositoryRequestQuery from './SearchBar.query';
+import "./SearchBar.scss";
 
 export const SearchBar = () => {
-    return (<>
-    <div className="searchBarContent shadow-sm p-3 mb-5 bg-white rounded">
-        
-    </div>
-    </>)
-}
+  const [owner, setOwner] = useState("");
+  const [repository, setRepository] = useState("");
+
+  const handleOwnerChange = (event: FormEvent<HTMLInputElement>): void => {
+    setOwner(event?.currentTarget?.value);
+  };
+
+  const handleRepositoryChange = (event: FormEvent<HTMLInputElement>): void => {
+    setRepository(event?.currentTarget?.value);
+  };
+
+  const searchForRepository = async (event: any): Promise<void> => {
+    if (event.charCode === 13) {
+      const { data } = await requestQuery(
+        repositoryRequestQuery(owner, repository)
+      );
+      console.log(data);
+    }
+  };
+
+  return (
+    <>
+      <div className="searchBarContent shadow-sm p-3 mb-5 bg-white rounded">
+        <input
+          className="userOrOrgInput"
+          value={owner}
+          onChange={handleOwnerChange}
+          placeholder="User or Organization"
+          type="text"
+        />
+        <input
+          type="text"
+          className="repoInput"
+          value={repository}
+          onKeyPress={searchForRepository}
+          onChange={handleRepositoryChange}
+          placeholder="Repository"
+        />
+      </div>
+    </>
+  );
+};
