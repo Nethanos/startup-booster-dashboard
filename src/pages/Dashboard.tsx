134e5220c2cb438690c5f7a0cc749fd53cccc277
Nethanos <n.santos1897@gmail.com>
2020-11-10 21:47:35 -0300

    [refactor] turning dashboard main component

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,6 +1,39 @@
-import React from 'react';
+import React from "react";
+import { SearchBar } from "../components/searchbar/SearchBar";
+import { Sidemenu } from "../components/sidemenu/Sidemenu";
+import "./Dashboard.scss";
 
 export const Dashboard = () => {
-    return (<> 
-    Dashboard works</>)
-}
+  return (
+    <>
+      <div className="mainContent">
+        <div className="sideMenu">
+          <Sidemenu></Sidemenu>
+        </div>
+
+        <div className="searchBar">
+          <SearchBar></SearchBar>
+        </div>
+
+        <div className="dashboard">
+          <div className="container">
+            <div className="row">
+              <div className="col-sm-12 align-items-center card">
+                <div>first area</div>
+              </div>
+              <div className="col-sm-12 col-md-6 mr-auto card align-items-center">
+                INFOBOX
+              </div>
+              <div className="col-sm-12 col-md-6 mr-auto card align-items-center">
+                INFOBOX
+              </div>
+              <div className="col-sm-12 align-items-center card">
+                SECOND AREA{" "}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
