fe870e727ab1be761aae20bb3729eba37866abad
Nethanos <n.santos1897@gmail.com>
2020-11-11 21:19:38 -0300

    [refactor] removing unnecessary imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
 import './App.scss';
 import React from 'react';
-import { Sidemenu } from './components/sidemenu/Sidemenu';
 import { Dashboard } from './pages/Dashboard';
-import  {SearchBar} from './components/searchbar/SearchBar';
 import './App.scss'
 
 function App() {
