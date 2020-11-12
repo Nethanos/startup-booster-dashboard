402a284391588d07d0d513e08efb3d884bae970b
Nethanos <n.santos1897@gmail.com>
2020-11-11 22:05:41 -0300

    [refactor] adjusting graphql query

--- src/components/searchbar/SearchBar.query.ts
+++ src/components/searchbar/SearchBar.query.ts
@@ -1,22 +1,22 @@
 export default function repositoryRequestQuery(owner: string, name: string): string {
     return `query getDocuments { 
       repository(owner: "micronaut-projects", name: "micronaut-core") {
-
-        Issues: issues(last: 100, filterBy: {states: CLOSED}) {
-          nodes {
-            createdAt,
-            closedAt
-          }
+         Issues: issues(last: 100, filterBy: {states: CLOSED}) {
+                totalCount,
+                nodes {
+                  createdAt,
+                  closedAt
+                }
         },
-        
-        MergedPullRequests: pullRequests(last: 100, states: MERGED) {
-          nodes {
-            createdAt,
-            mergedAt,
-            additions,
-            deletions
-          }
-        },      
-  		}
+         MergedPullRequests: pullRequests(last: 100, states: MERGED) {
+              nodes {
+                createdAt,
+                mergedAt,
+                additions,
+                deletions
+              }
+         },      
+          
+      }
     }`;
   }
