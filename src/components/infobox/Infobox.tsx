7c2a5152dd43ec17293a21736a991f24fa761be1
Nethanos <n.santos1897@gmail.com>
2020-11-11 22:03:15 -0300

    [feature] creating logic to receave function and information in infobox

--- src/components/infobox/Infobox.tsx
+++ src/components/infobox/Infobox.tsx
@@ -1,20 +1,28 @@
-import React from 'react';
-import './Infobox.scss';
-
+import React, { useEffect, useState } from "react";
+import mountTimeString from "../../helpers/TimeStringFactory";
+import "./Infobox.scss";
 
 type InfoboxProps = {
-    title: string;
-}
+  title: string;
+  info?: any;
+  infoFunction: Function;
+};
 
 export const Infobox = (infoBoxProps: InfoboxProps) => {
+  const [timeString, setTimeString] = useState("");
 
-    return (<>
-    
-   <div className="infoboxCard">
-        <div className="header">
-            {infoBoxProps.title}
-        </div>
-   </div>
+  useEffect(() => {
+    setTimeString(
+      mountTimeString(infoBoxProps.infoFunction(infoBoxProps.info))
+    );
+  }, [infoBoxProps.info]);
 
-    </>)
-}
+  return (
+    <>
+      <div className="infoboxCard">
+        <div className="header">{infoBoxProps.title}</div>
+        <div>{timeString}</div>
+      </div>
+    </>
+  );
+};
