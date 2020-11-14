904e1df5cba0c88f76e649a75a911058c220f7d3
Nethanos <n.santos1897@gmail.com>
2020-11-14 10:29:38 -0300

    [feature]: adding function to get date without hours or any unnecessary info to compare

--- src/helpers/DateConverter.ts
+++ src/helpers/DateConverter.ts
@@ -1,38 +1,45 @@
 /**
  * This code was originally implemented by @fargolas in pure JS https://gist.github.com/flangofas/714f401b63a1c3d84aaa
- * 
+ *
  * This is a typescript adapted to project version.
  */
 
 export interface DateConverted {
-    days: number,
-    hours: number,
-    minutes: number,
-    seconds: number;
+  days: number;
+  hours: number;
+  minutes: number;
+  seconds: number;
 }
 
-export default function convertMiliseconds(miliseconds: number, format?: string): number | DateConverted  {
-    let dateConverted = {} as DateConverted;
-    
-    const totalSeconds = Math.floor(miliseconds / 1000);
-    const totalMinutes = Math.floor(totalSeconds / 60);
-    const totalHours = Math.floor(totalMinutes / 60);
-    dateConverted.days = Math.floor(totalHours / 24);
-  
-    dateConverted.seconds = totalSeconds % 60;
-    dateConverted.minutes = totalMinutes % 60;
-    dateConverted.hours = totalHours % 24;
-    
-    switch(format) {
-      case 's':
-          return totalSeconds;
-      case 'm':
-          return totalMinutes;
-      case 'h':
-          return totalHours;
-      case 'd':
-          return dateConverted.days;
-      default:
-          return dateConverted;
-    }
-  };
+export default function convertMiliseconds(
+  miliseconds: number,
+  format?: string
+): number | DateConverted {
+  let dateConverted = {} as DateConverted;
+
+  const totalSeconds = Math.floor(miliseconds / 1000);
+  const totalMinutes = Math.floor(totalSeconds / 60);
+  const totalHours = Math.floor(totalMinutes / 60);
+  dateConverted.days = Math.floor(totalHours / 24);
+
+  dateConverted.seconds = totalSeconds % 60;
+  dateConverted.minutes = totalMinutes % 60;
+  dateConverted.hours = totalHours % 24;
+
+  switch (format) {
+    case "s":
+      return totalSeconds;
+    case "m":
+      return totalMinutes;
+    case "h":
+      return totalHours;
+    case "d":
+      return dateConverted.days;
+    default:
+      return dateConverted;
+  }
+}
+
+export function getExactDate(date: Date) {
+  return `${date.getDate()}-${date.getMonth()}-${date.getFullYear()}`;
+}
