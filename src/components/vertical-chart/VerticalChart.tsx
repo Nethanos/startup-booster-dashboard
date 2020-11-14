09f4a3641ae4560046186275431a9d58a7173f9c
Nethanos <n.santos1897@gmail.com>
2020-11-14 10:28:51 -0300

    [feature][format]: creating id for chart component and formatting document

--- src/components/vertical-chart/VerticalChart.tsx
+++ src/components/vertical-chart/VerticalChart.tsx
@@ -1,25 +1,24 @@
-import React, { useEffect, useState } from "react";
-import Chart, { ChartTooltipLabelColor, ChartTooltipModel } from "chart.js";
+import React, { useEffect } from "react";
+import Chart from "chart.js";
 
 type VerticalChartProps = {
   chartData: any;
   buildChartFunction: Function;
+  title: string;
+  chartId: string;
 };
 
 let chart = {} as Chart;
 
 export const VerticalChart = (props: VerticalChartProps) => {
+  function mountChart(chartData: any) {
+    const chartRef = document.getElementById(props.chartId) as HTMLCanvasElement;
 
-  
-function mountChart(chartData: any) {
-  const chartRef = document.getElementById("prChart") as HTMLCanvasElement;
-
-  if(chart.canvas){
-    chart.destroy();
+    if (chart.canvas) {
+      chart.destroy();
+    }
+    chart = new Chart(chartRef, props.buildChartFunction(chartData));
   }
-    chart = new Chart(chartRef, props.buildChartFunction(chartData) );
-  }
- 
 
   useEffect(() => {
     mountChart(props?.chartData);
@@ -27,7 +26,12 @@ function mountChart(chartData: any) {
 
   return (
     <>
-      <canvas id="prChart"></canvas>
+      <div className="card">
+        <div className="card-header">
+          {props.title}
+        </div>
+        <canvas id={props.chartId}></canvas>
+      </div>
     </>
   );
 };
